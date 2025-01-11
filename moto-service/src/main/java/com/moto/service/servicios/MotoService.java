86be33f81375517d0d5252529bc2fcd99590679c
DiegoDoria <retrexretrex@hotmail.com>
2025-01-11 09:34:54 -0500

    Rest template y Feign Client

---
+++ moto-service/src/main/java/com/moto/service/servicios/MotoService.java
@@ -0,0 +1,34 @@
+package com.moto.service.servicios;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.moto.service.entidades.Moto;
+import com.moto.service.repositorio.MotoRepository;
+
+@Service
+public class MotoService {
+	
+	@Autowired
+	public MotoRepository motoRepository;
+	
+	public List<Moto> getAll(){
+		return motoRepository.findAll();
+	}
+	
+	public Moto getCarroById(int id) {
+		return motoRepository.findById(id).orElse(null);
+	}
+	
+	public Moto save(Moto carro) {
+		Moto nuevoUsuario = motoRepository.save(carro);
+		return nuevoUsuario;
+	}
+	
+	public List<Moto> byUsuarioId(int usuarioId){
+		return motoRepository.findByUsuarioId(usuarioId);
+	}
+
+}
